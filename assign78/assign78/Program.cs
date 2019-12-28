512075bd57ab8af7a045e115d75d13fae69a5195
Jared <jaredejaneway@gmail.com>
2019-12-28 17:15:35 -0600

    This adds the C# file we will be working from

---
+++ assign78/assign78/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace assign78
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
